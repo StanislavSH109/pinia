db2e7193c0952878f0a2d4864d47822e050798f8
Stanislav Shastov <shastov.stas1991@gmail.com>
2025-07-22 15:02:50 +0300

    +++

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
+<template>
+  <FirstComponent />
+  <SecondComponent />
+</template>
+
+
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
 import HelloWorld from './components/HelloWorld.vue';
 import FirstComponent from './components/FirstComponent.vue';
 import SecondComponent from './components/SecondComponent.vue';
 </script>
-
-<template>
-  <FirstComponent />
-  <SecondComponent />
-</template>
 
 <style scoped></style>
