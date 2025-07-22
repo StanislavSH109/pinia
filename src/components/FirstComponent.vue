34b9a97b2c12d59ec4fc7799f8698b1002f3339e
Stanislav Shastov <shastov.stas1991@gmail.com>
2025-07-22 14:45:21 +0300

    +++

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>First Component</h1>
+  <p>My Count: {{ store.count }}</p>
+  <p>My DoubleCount: {{ store.doubleCount }}</p>
+  <button @click="store.increment">Добавить</button>
+</template>
+
+<script lang="ts" setup>
+import { useCounterStore } from '@/stores/counter';
+
+const store = useCounterStore()
+
+</script>
+
+<style scoped>
+
+</style>
